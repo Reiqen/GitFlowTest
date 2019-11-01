10b4c5328ccded3aaf1f53c95572f134d82411c1
Roman Kuznetcov <reiqen@yahoo.com>
2019-11-01 23:27:14 +0400

    Add interface for missing students

---
+++ src/mainpackage/testobjects/Missing.java
@@ -0,0 +1,17 @@
+package mainpackage.testobjects;
+
+/** Defines reasons for missing classes.
+ * 
+ * @author Roman Kuznetcov
+ *
+ */
+
+public interface Missing {
+
+	/**
+	 * Defines a desease name which is the reason of missing classes.
+	 */
+	
+	String getSickName();
+	
+}
