5fea0397540cf460064053a6978587e8a7519c28
Roman Kuznetcov <reiqen@yahoo.com>
2019-11-01 21:51:35 +0400

    Add new features Add a behavior for students (new interface) Add methods' implementation for students

---
+++ src/mainpackage/testobjects/Behavable.java
@@ -0,0 +1,15 @@
+package mainpackage.testobjects;
+
+public interface Behavable {
+
+	/** Every student must raise a hand when they know an answer.
+	 * 
+	 */
+	void raiseHand();
+	
+	/**
+	 * Every student must shout a mark he/she received to a teacher.
+	 */
+	void shoutMark(int mark);
+	
+}
--- src/mainpackage/testobjects/Student.java
+++ src/mainpackage/testobjects/Student.java
@@ -1,10 +1,11 @@
 package mainpackage.testobjects;
 
-public class Student {
+public class Student implements Behavable {
 
 	private String name;
 	private String surname;
 	private int age;
+	private boolean handRaised;
 	
 	public String getName() {
 		return name;
@@ -31,11 +32,19 @@ public class Student {
 	}
 
 	@Override
+	public void raiseHand() {
+		handRaised = true;
+	}
+
+	@Override
+	public void shoutMark(int mark) {
+		System.out.println("My mark is " + mark);
+	}
+
+	@Override
 	public String toString() {
 		return "Student [name=" + name + ", surname=" + surname + ", age=" + age + "]";
 	}
-
-	
 	
 	
 }
